7dad7029775888274d8b0f486972d7125638fd8f
LeonardoMSA <leonardomsazevedo@gmail.com>
2023-09-18 22:42:00 -0300

    Created Driver model's fields

---
+++ DatabaseProject/models.py
@@ -0,0 +1,30 @@
+import datetime
+from DatabaseProject import db
+
+class Driver(db.Model):
+
+    __tablename__ = 'drivers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    cpf = db.Column(db.String(16), unique=True, index=True)
+    name = db.Column(db.String(100), index=True)
+    email = db.Column(db.String(64), unique=True, index=True)
+    carPlate = db.Column(db.String(16), unique=True, index=True)
+    phoneNumber = db.Column(db.String(16), unique=True)
+    CEP = db.Column(db.String(32))
+    birthDate = db.Column(db.DateTime)
+    paymentMethod = db.Column(db.String)
+    soleOccupation = db.Column(db.Boolean)
+    appDriverMonths = db.Column(db.Integer)
+    otherApps = db.Column(db.ARRAY(str))
+    uberCategory = db.Column(db.String)
+    rentalCar = db.Column(db.Boolean)
+    dailyWorkTime = db.Column(db.Integer)
+    weekDays = db.Column(db.ARRAY(str))
+    uberRating = db.Column(db.Float)
+    totalTrips = db.Column(db.Integer)
+    carModel = db.Column(db.String(64))
+    workArea = db.Column(db.String(100))
+    workInterest = db.Column(db.Boolean)
+
+
