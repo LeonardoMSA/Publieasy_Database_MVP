e775efc2170691599785e28549b80987ffe40b13
Lucas Fernandes <lfaa2057@gmail.com>
2024-03-20 15:57:01 -0300

    att. mobile vers.

--- DatabaseProject/static/assets/js/script.js
+++ DatabaseProject/static/assets/js/script.js
@@ -1,3 +1,18 @@
-function smoothScrolling(){
+"use strict"
 
+function screenChanges(screenWidth)
+{
+    const sloganTitle = document.querySelector('.slogan-title');
+    const sloganSubTitle = document.querySelector('.sub-slogan');
+
+
+    if (screenWidth.matches) 
+        sloganTitle.innerHTML = sloganTitle.textContent.replace('<br>', '');
+    
+    if (screenWidth.matches) 
+        sloganSubTitle.innerHTML = sloganSubTitle.textContent.replace('<br>', '');
+    
 }
+
+screenChanges(window.matchMedia("(max-width: 600px)"));
+window.matchMedia("(max-width: 600px)").addListener(screenChanges);
